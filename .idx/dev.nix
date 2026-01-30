05bcd431a29cc79ea391e494c989a951a1cd4c24
omna25data-afk <user@example.com>
2026-01-30 22:47:47 +0300

    Add Project IDX configuration (dev.nix)

---
+++ .idx/dev.nix
@@ -0,0 +1,18 @@
+{
+  "name": "qlm_syani_app",
+  "idx": {
+    "extensions": [
+      "Dart-Code.flutter",
+      "Dart-Code.dart-code",
+      "robert-brunhage.flutter-riverpod-snippets",
+      "nash.awesome-flutter-snippets",
+      "BendixMa.riverpod-snippets"
+    ],
+    "workspace": {
+      "onCreate": {
+        "pub-get": "flutter pub get",
+        "build_runner": "dart run build_runner build --delete-conflicting-outputs"
+      }
+    }
+  }
+}
