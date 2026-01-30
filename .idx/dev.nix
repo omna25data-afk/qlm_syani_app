e9d0afc3302b27237c1a9bcbb35815da5b0ae180
omna25data-afk <user@example.com>
2026-01-30 22:48:30 +0300

    Fix IDX config syntax (Nix language) and add extensions

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,18 +1,50 @@
-{
-  "name": "qlm_syani_app",
-  "idx": {
-    "extensions": [
-      "Dart-Code.flutter",
-      "Dart-Code.dart-code",
-      "robert-brunhage.flutter-riverpod-snippets",
-      "nash.awesome-flutter-snippets",
-      "BendixMa.riverpod-snippets"
-    ],
-    "workspace": {
-      "onCreate": {
-        "pub-get": "flutter pub get",
-        "build_runner": "dart run build_runner build --delete-conflicting-outputs"
-      }
-    }
-  }
+{pkgs}: {
+  channel = "stable-24.05";
+  packages = [
+    pkgs.jdk17
+    pkgs.unzip
+  ];
+  idx.extensions = [
+    "Dart-Code.flutter"
+    "Dart-Code.dart-code"
+    "robert-brunhage.flutter-riverpod-snippets"
+    "nash.awesome-flutter-snippets"
+    "BendixMa.riverpod-snippets"
+  ];
+  idx.workspace = {
+    onCreate = {
+      pub-get = "flutter pub get";
+      build-runner = "dart run build_runner build --delete-conflicting-outputs";
+    };
+  };
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "flutter"
+          "run"
+          "--machine"
+          "-d"
+          "web-server"
+          "--web-hostname"
+          "0.0.0.0"
+          "--web-port"
+          "$PORT"
+        ];
+        manager = "flutter";
+      };
+      android = {
+        command = [
+          "flutter"
+          "run"
+          "--machine"
+          "-d"
+          "android"
+          "-d"
+          "localhost:5555"
+        ];
+        manager = "flutter";
+      };
+    };
+  };
 }
